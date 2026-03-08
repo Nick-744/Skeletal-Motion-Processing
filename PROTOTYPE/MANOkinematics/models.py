648fe02d154bf5dd7da78cb5e4380dac2ed3f1ba
G. Nikolaos X. <100804668+Nick-744@users.noreply.github.com>
2026-03-08 21:25:06 +0200

    models.py documentation/comments

--- PROTOTYPE/MANOkinematics/models.py
+++ PROTOTYPE/MANOkinematics/models.py
@@ -45,39 +45,45 @@ class KinematicModel():
     with open(model_path, 'rb') as f:
       params = pickle.load(f)
 
+      # Load PCA components for pose (used to reduce the dimensionality of the pose space)
       self.pose_pca_basis = params['pose_pca_basis']
       self.pose_pca_mean  = params['pose_pca_mean' ]
 
+      # Load the joint regressor matrix (calculates joint 3D locations from mesh vertices)
       self.J_regressor = params['J_regressor']
 
+      # Load skinning weights (defines how much each joint's movement affects each vertex)
       self.skinning_weights = params['skinning_weights']
 
       self.mesh_pose_basis  = params['mesh_pose_basis' ] # Pose blend shape
       self.mesh_shape_basis = params['mesh_shape_basis']
-      self.mesh_template    = params['mesh_template'   ]
+      self.mesh_template    = params['mesh_template'   ] # T-pose
 
       self.faces =  params['faces']
 
-      self.parents = params['parents']
+      self.parents = params['parents'] # Kinematic tree
 
     self.n_shape_params = self.mesh_shape_basis.shape[-1]
     self.scale          = scale
 
     self.armature = armature
     self.n_joints = self.armature.n_joints
+
     self.pose  = np.zeros((self.n_joints, 3))
     self.shape = np.zeros(self.mesh_shape_basis.shape[-1])
-    self.verts = None
-    self.J = None
-    self.R = None
+
+    self.verts     = None
+    self.J         = None
+    self.R         = None
     self.keypoints = None
 
+    # Create an extended joint regressor to calculate extra keypoints (fingertips)
     self.J_regressor_ext = np.zeros([self.armature.n_keypoints, self.J_regressor.shape[1]])
     self.J_regressor_ext[:self.armature.n_joints] = self.J_regressor
     for (i, v) in enumerate(self.armature.keypoints_ext):
       self.J_regressor_ext[i + self.armature.n_joints, v] = 1
 
-    self.update()
+    self.update() # Run the Forward Kinematics and Linear Blend Skinning...
 
     return;
 
@@ -109,20 +115,24 @@ class KinematicModel():
       Keypoints coordinates of the model, scale applied.
     '''
 
+    # If an absolute axis-angle pose is provided...
     if pose_abs is not None:
       self.pose = pose_abs
+    
+    # Otherwise, if PCA pose coefficients are provided...
     elif pose_pca is not None:
+      # Reconstruct the absolute pose from PCA
       self.pose = np.dot(
         np.expand_dims(pose_pca, 0), self.pose_pca_basis[:pose_pca.shape[0]]
       )[0] + self.pose_pca_mean
       self.pose = np.reshape(self.pose, [self.n_joints - 1, 3])
-      if pose_glb is None:
-        pose_glb = np.zeros([1, 3])
+
+      if pose_glb is None: pose_glb = np.zeros([1, 3])
       pose_glb  = np.reshape(pose_glb, [1, 3])
       self.pose = np.concatenate([pose_glb, self.pose], 0)
 
-    if shape is not None:
-      self.shape = shape
+    # If shape parameters (beta) are provided...
+    if shape is not None: self.shape = shape
 
     return self.update();
 
@@ -139,29 +149,42 @@ class KinematicModel():
       Keypoints coordinates of the model, scale applied.
     '''
 
-    verts  = self.mesh_template + self.mesh_shape_basis.dot(self.shape)
+    # Apply shape coefficients to the base template
+    verts = self.mesh_template + self.mesh_shape_basis.dot(self.shape)
+
     self.J = self.J_regressor.dot(verts)
     self.R = self.rodrigues(self.pose.reshape((-1, 1, 3)))
-    G      = np.empty((self.n_joints, 4, 4))
-    G[0]   = self.with_zeros(np.hstack((self.R[0], self.J[0, :].reshape([3, 1]))))
+
+    # FORWARD KINEMATICS: Compute global transformation matrices for each joint
+    G    = np.empty((self.n_joints, 4, 4))
+    # Root joint (index 0):
+    G[0] = self.with_zeros(np.hstack((self.R[0], self.J[0, :].reshape([3, 1]))))
+
+    # Iterate through the rest of the joints
     for i in range(1, self.n_joints):
+      # Global Transform = Parent Global Transform * Local Transform
       G[i] = G[self.parents[i]].dot(self.with_zeros(
           np.hstack([
             self.R[i],
             (self.J[i, :] - self.J[self.parents[i], :]).reshape([3, 1])
           ])
       ))
+
+    # Deformation required to move vertices from the T-pose
     G = G - self.pack(np.matmul(
         G,
         np.hstack([self.J, np.zeros([self.n_joints, 1])]).reshape([self.n_joints, 4, 1])
     ))
 
-    T     = np.tensordot(self.skinning_weights, G, axes = [[1], [0]])
+    # LINEAR BLEND SKINNING (LBS)
+    T = np.tensordot(self.skinning_weights, G, axes = [[1], [0]])
+
     verts = np.hstack((verts, np.ones([verts.shape[0], 1])))
 
     self.verts     = np.matmul(T, verts.reshape([-1, 4, 1])).reshape([-1, 4])[:, :3]
     self.keypoints = self.J_regressor_ext.dot(self.verts)
 
+    # SCALING
     self.verts     *= self.scale
     self.keypoints *= self.scale
 
@@ -184,25 +207,28 @@ class KinematicModel():
     '''
 
     theta = np.linalg.norm(r, axis = (1, 2), keepdims = True)
-
-    # Avoid zero divide
-    theta = np.maximum(theta, np.finfo(np.float64).eps)
+    theta = np.maximum(theta, np.finfo(np.float64).eps) # Avoid zero divide
 
     r_hat   = r / theta
     cos     = np.cos(theta)
     z_stick = np.zeros(theta.shape[0])
-    m       = np.dstack([
-      z_stick,        -r_hat[:, 0, 2], r_hat[:, 0, 1],
-      r_hat[:, 0, 2],  z_stick,       -r_hat[:, 0, 0],
+
+    m = np.dstack([
+       z_stick,       -r_hat[:, 0, 2], r_hat[:, 0, 1],
+       r_hat[:, 0, 2], z_stick,       -r_hat[:, 0, 0],
       -r_hat[:, 0, 1], r_hat[:, 0, 0], z_stick]
     ).reshape([-1, 3, 3])
+
     i_cube = np.broadcast_to(
       np.expand_dims(np.eye(3), axis = 0), [theta.shape[0], 3, 3]
     )
-    A = np.transpose(r_hat, axes = [0, 2, 1])
-    B = r_hat
+
+    A   = np.transpose(r_hat, axes = [0, 2, 1])
+    B   = r_hat
     dot = np.matmul(A, B)
-    R   = cos * i_cube + (1 - cos) * dot + np.sin(theta) * m
+
+    # Rodrigues' rotation formula
+    R = cos * i_cube + (1 - cos) * dot + np.sin(theta) * m
 
     return R;
 
