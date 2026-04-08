72c8e8ceab53de76641f33112d00603bafb154fe
G. Nikolaos X. <100804668+Nick-744@users.noreply.github.com>
2026-04-08 04:22:09 +0300

    Ball choices!

--- PROTOTYPE/my_hands_game/Assets/Scripts/BraceletMenuController.cs
+++ PROTOTYPE/my_hands_game/Assets/Scripts/BraceletMenuController.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class ProceduralBracelet : MonoBehaviour
+public class BraceletMenuController : MonoBehaviour
 {
     [Header("Dependencies")]
     [Tooltip("Drag the GameObject holding the ManoLiveReceiver script here.")]
@@ -14,8 +14,15 @@ public class ProceduralBracelet : MonoBehaviour
     [Tooltip("Position Offset")]
     public Vector3 braceletPositionOffset = new Vector3(-0.15f, 0f, 0f);
 
+    [Header("Balls Settings")]
+    [Tooltip("Distance of the balls from the center of the bracelet")]
+    public float ballRadius = 0.5f;
+    [Tooltip("Size of each ball")]
+    public float ballScale  = 0.2f;
+
     private GameObject braceletVisual;
     private GameObject band;
+    private GameObject[] balls = new GameObject[3];
 
     void Start() { CreateYellowBracelet(); }
 
@@ -31,10 +38,31 @@ public class ProceduralBracelet : MonoBehaviour
         band.transform.localEulerAngles = braceletRotationOffset;
         band.transform.localPosition    = braceletPositionOffset;
         
-        band.GetComponent<Renderer>().material.color = Color.yellow; 
+        band.GetComponent<Renderer>().material.color = Color.yellow;
         
         // Remove physics collider
-        Destroy(band.GetComponent<Collider>()); 
+        Destroy(band.GetComponent<Collider>());
+
+        // Create the 3 Balls wrapped around
+        for (int i = 0; i < 3; i++)
+        {
+            balls[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            balls[i].transform.SetParent(braceletVisual.transform);
+
+            // Calculate angle
+            float angleDegrees = i * 90f;
+            float angleRadians = angleDegrees * Mathf.Deg2Rad;
+
+            Vector3 cylinderSpacePos = new Vector3(Mathf.Cos(angleRadians) * ballRadius, 0f, Mathf.Sin(angleRadians) * ballRadius);
+            Vector3 rotatedOrbitPos  = Quaternion.Euler(braceletRotationOffset) * cylinderSpacePos;
+            
+            balls[i].transform.localPosition = rotatedOrbitPos + braceletPositionOffset;
+            balls[i].transform.localScale    = Vector3.one * ballScale;
+            balls[i].GetComponent<Renderer>().material.color = Color.green;
+            
+            // Remove physics collider
+            Destroy(balls[i].GetComponent<Collider>()); 
+        }
     }
 
     void Update()
